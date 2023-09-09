ccbbf7b7b188395d3e49309316c76280eec72eef
4-NoLucK-4 <62057317+4-NoLucK-4@users.noreply.github.com>
2023-09-09 20:14:39 +0200

    V0.05 Embed Prés. Aquila

--- src/commands/infos/embed.ts
+++ src/commands/infos/embed.ts
@@ -11,20 +11,18 @@ export default new Command({
 		const embed = new EmbedBuilder()
 		.setColor('White')
 		.setTitle('Aquila Studio')
-		.setURL('https://discord.js.org/')
-		.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-		.setDescription('Some description here')
-		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+		.setURL('https://github.com/Nolucci/Aquila-Bot/tree/master')
+		.setAuthor({ name: 'Aquila Bot', url: 'https://github.com/Nolucci/Aquila-Bot/tree/master' })
+		.setDescription('Studio créé en 2023 par des étudiants en informatique intéressés par la création de jeux vidéos.')
+		.setThumbnail('https://raw.githubusercontent.com/Nolucci/Aquila-Bot/master/Ressources/Aquila_Logo_grand.png')
 		.addFields(
-			{ name: 'Regular field title', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
+			{ name: 'Adhérents au Studio :', value: '\u2000' },
+			{ name: 'Franceskin Nathan', value: 'Dev', inline: true},
+			{ name: 'Dumeniaud Kilian', value: 'Dev', inline: true },
 		)
-		.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-		.setImage('https://i.imgur.com/AfFp7pu.png')
+		//.setImage('https://raw.githubusercontent.com/Nolucci/Aquila-Bot/master/Ressources/Aquila_Logo_grand.png')
 		.setTimestamp()
-		.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+		.setFooter({ text: 'Nolucci', iconURL: 'https://raw.githubusercontent.com/Nolucci/Aquila-Bot/master/Ressources/nolucci_pp.png'});
 
         interaction.reply({embeds: [embed]});
 
